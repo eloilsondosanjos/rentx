d8c0400f383b8cc2d615ccd8dac931b8d14a1f2b
eloilsondosanjos <eloilson.ageedu@gmail.com>
2021-08-02 21:41:51 -0300

    Refatorado a estrutura da aplicação

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,8 +1,8 @@
 import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDTO';
-import { User } from '@modules/accounts/entities/User';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { getRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
 
-import { IUsersRepository } from '../IUsersRepository';
 
 class UsersRepository implements IUsersRepository {
   private repository: Repository<User>;
